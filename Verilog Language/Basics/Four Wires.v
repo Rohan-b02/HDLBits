3176895bf97382abc7dec7406e337ff269641dcb
Rohan Boge <146671579+Rohan-b02@users.noreply.github.com>
2024-02-08 01:37:15 +0530

    Create Four Wires.v

---
+++ Verilog Language/Basics/Four Wires.v
@@ -0,0 +1,15 @@
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+    
+    assign {w,x,y,z}={a,b,b,c};
+    
+    // or 
+    /*
+     assign w = a;
+     assign x = b;
+     assign y = b;
+     assign z = c 
+    */
+
+endmodule
